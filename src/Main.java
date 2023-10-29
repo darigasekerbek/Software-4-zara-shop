4e46d7e5768822d14f4f76ece3ca75b5153ea452
Abylai <221714@astanait.edu.kz>
2023-10-29 19:59:58 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Shop shop = ShopFactory.createShop();
+
+        ShopEmployee adelina = new ShopEmployee("Adelina");
+        ShopEmployee madiyar = new ShopEmployee("Madiyar");
+        ShopEmployee ramazan = new ShopEmployee("Ramazan");
+
+        shop.addObserver(adelina);
+        shop.addObserver(madiyar);
+        shop.addObserver(ramazan);
+
+        shop.openVacancy("Commercial Specialist.");
+
+        madiyar.applyForJob(shop);
     }
 }
